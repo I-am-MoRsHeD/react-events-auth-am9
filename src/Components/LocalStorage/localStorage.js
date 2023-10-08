327ca2824eac91fcc27d9f69b3a6346abbd52b14
MoRsHeD <mdmorshed0187@gmail.com>
2023-10-08 18:52:45 +0600

    set cart in local storage

---
+++ src/Components/LocalStorage/localStorage.js
@@ -0,0 +1,25 @@
+
+
+const getStoredItems  = () =>{
+    const storedItem = localStorage.getItem('cart');
+    if(storedItem){
+        return JSON.parse(storedItem);
+    }
+    return [];
+}
+
+
+
+const setItems = (id) =>{
+    const storedItems = getStoredItems();
+    const isExist = storedItems.find(cardId => cardId === id);
+
+    if(!isExist){
+        storedItems.push(id);
+        localStorage.setItem('cart', storedItems);
+    }
+}
+
+export{ getStoredItems, setItems};
+
+
--- src/Pages/ServiceDetails.jsx
+++ src/Pages/ServiceDetails.jsx
@@ -2,10 +2,15 @@
 import React, { useEffect, useState } from 'react';
 import { useLoaderData, useParams } from 'react-router-dom';
 import Navbar from '../Components/Navbar/Navbar';
+import Aos from 'aos';
+import 'aos/dist/aos.css';
+import { setItems } from '../Components/LocalStorage/localStorage';
 
 const ServiceDetails = () => {
+    Aos.init();
     const [info, setInfo] = useState('');
     const { id } = useParams();
+    const idnt = parseInt(id);
     const data = useLoaderData();
 
     useEffect(() => {
@@ -13,17 +18,24 @@ const ServiceDetails = () => {
         setInfo(findData)
     }, [id, data])
     const { title, image_url, desc, price } = info;
+
+    const handleCart = () =>{
+        setItems(idnt);
+        
+    }
     return (
         <div>
             <Navbar></Navbar>
             <div className='bg-teal-500 p-10'>
-                <div className='flex justify-between h-screen border-2 border-dotted border-amber-200 p-2 items-center'>
-                    <div className='flex-1 text-center space-y-10'>
+                <div className='flex justify-between gap-5 h-screen border-2 border-dotted border-amber-200 p-2 items-center'>
+                    <div data-aos="fade-right" className='flex-1 text-center space-y-10'>
                         <h2 className="text-4xl font-bold">{title}</h2>
-                        <p>{desc}</p>
-                        <button className='bg-gray-300 w-1/2 p-2 rounded-lg hover:bg-red-500'>Price: ${price}</button>
+                        <p className='text-xl'>{desc}</p>
+                        <button
+                        onClick={handleCart}
+                        className='bg-gray-300 w-1/2 p-2 rounded-lg hover:bg-red-500'>Add to cart: ${price}</button>
                     </div>
-                    <div className='w-1/2 '>
+                    <div data-aos="fade-left" className='w-1/2 '>
                         <img src={image_url} alt="" />
                     </div>
                 </div>
